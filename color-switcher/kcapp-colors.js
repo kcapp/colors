8ba31e4326535557e7fd100f06a2d4de27b4b15f
thordy <thordendre@gmail.com>
2019-03-31 12:36:15 +0200

    Switched to use the kcapp-sio-client, and some cleanup

--- color-switcher/kcapp-colors.js
+++ color-switcher/kcapp-colors.js
@@ -1,77 +1,45 @@
-var axios = require('axios');
-var io = require("socket.io-client");
+var debug = require('debug')('kcapp-color-switcher:main');
+var led = require("./led_util")(17, 22, 24);
 
-var led = require("./led_util");
-led.setup(17, 22, 24);
-
-var BASE_URL = "http://localhost";
-var API_URL = BASE_URL + ":8001";
-var socket = io(BASE_URL + ":3000/active");
-
-function setupLegNamespace(legId) {
-    // Connect to leg namespace, so we can cycle lights between player throws
-    var legSocket = io(BASE_URL + ":3000/legs/" + legId);
-    legSocket.on('score_update', scoreUpdate);
-    legSocket.on('leg_finished', legFinished);
-    legSocket.on('possible_throw', possibleThrow);
-}
-
-function scoreUpdate(data) {
-    for (var i = 0; i < data.players.length; i++) {
-        var player = data.players[i];
-        if (player.is_current_player) {
-            led.setColor(player.player.color);
-        }
-    }
-}
-function possibleThrow(data) {
-    if (data.is_finished) {
-        led.blink('#00ff00', 3000);
-    }
-}
-
-function legFinished(data) {
-    console.log("Leg finished, moving to next: " + data.new_leg_id);
-    setupLegNamespace(data.new_leg_id);
-}
-
-socket.on('new_match', (data) => {
+function connectToMatch(data) {
     var match = data.match;
-    var legId = match.current_leg_id;
-    if (match.venue.id === 4) {
-        // TODO add a generic "venue_configuration" to avoid hardcoding this here
-        axios.get(API_URL + "/leg/" + legId + "/players")
-            .then(response => {
-                var players = response.data;
+    if (match.venue.id === kcapp.DART_REIDAR_VENUE_ID) {
+        var legId = match.current_leg_id;
+        debug(`Connected to match ${match.id}`);
+
+        kcapp.connectLegNamespace(legId, (socket) => {
+            socket.on('score_update', (data) => {
+                var player = socket.currentPlayer.player;
+                debug("Setting color for " + player.name + " = " + player.color);
                 led.setColor(players[0].player.color);
-            }).catch(error => {
-                console.log(error);
             });
-        setupLegNamespace(legId);
-    } else {
-        console.log("Not setting color for match played at " + match.venue.name);
-    }
-});
 
-socket.on('warmup_started', (data) => {
-    var matchId = data.match.id;
-    axios.get(API_URL + '/match/' + matchId)
-        .then(response => {
-            var match = response.data;
-            if (match.venue.id === 4) {
-                setupLegNamespace(match.current_leg_id);
-            }
-        }).catch(error => {
-            console.log(error);
+            socket.on('leg_finished', (data) => {
+                var match = data.match;
+                if (match.is_finished) {
+                    debug("Disabling lights in 5s");
+                    setTimeout(() => led.turnOff(), 5000);
+                } else {
+                    debug("Blinking lights for 4s");
+                    led.blink('#00ff00', 4000);
+                }
+            });
+
+            var player = socket.currentPlayer.player;
+            debug("Setting color for " + player.name + " = " + player.color);
+            led.setColor(players[0].player.color);
         });
-});
-
-socket.on('leg_finished', (data) => {
-    var match = data.match;
-    if (match.is_finished) {
-        console.log("Disabling lights in 5s");
-        setTimeout(() => led.turnOff(), 5000);
     }
+}
+
+var kcapp = require('kcapp-sio-client/kcapp')("localhost", 3000);
+kcapp.connect(() => {
+    kcapp.on('new_match', (data) => {
+        connectToMatch(data);
+    });
+    kcapp.on('warmup_started', (data) => {
+        connectToMatch(data);
+    });
 });
 
-console.log("Waiting for events to change colors...")
+debug("Waiting for events to change colors...")
--- color-switcher/led_util.js
+++ color-switcher/led_util.js
@@ -1,25 +1,24 @@
+var debug = require('debug')('kcapp-color-switcher:led');
 var Gpio = require('pigpio').Gpio;
 
-var PINS = {};
-
+/**
+ * Write values for each pin to the given values
+ * @param {object} rgb - Object containing r,g,b values to set
+ */
 function write(rgb) {
     if (rgb && rgb != null) {
-        console.log("Writing lights " + JSON.stringify(rgb));
-        PINS.RED.pwmWrite(rgb.r);
-        PINS.GREEN.pwmWrite(rgb.g);
-        PINS.BLUE.pwmWrite(rgb.b);
+        debug("Writing lights " + JSON.stringify(rgb));
+        this.PINS.RED.pwmWrite(rgb.r);
+        this.PINS.GREEN.pwmWrite(rgb.g);
+        this.PINS.BLUE.pwmWrite(rgb.b);
     }
 }
 
-exports.setup = function (redGPIO, greenGPIO, blueGPIO) {
-    PINS = {
-        RED: new Gpio(redGPIO, { mode: Gpio.OUTPUT }),
-        GREEN: new Gpio(greenGPIO, { mode: Gpio.OUTPUT }),
-        BLUE: new Gpio(blueGPIO, { mode: Gpio.OUTPUT })
-    }
-}
-
-exports.hexToRGB = function (hex) {
+/**
+ * Convert the given hex color to RGB
+ * @param {string} hex - Hex to convert
+ */
+exports.hexToRGB = (hex) => {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
         r: parseInt(result[1], 16),
@@ -28,36 +27,63 @@ exports.hexToRGB = function (hex) {
     } : null;
 }
 
-exports.setColor = function (color) {
+/**
+ * Set the LEDs to the given hex color
+ * @param {string} color - Hex color to set
+ */
+exports.setColor = (color) => {
     rgb = this.hexToRGB(color);
     if (rgb === null) {
-        console.log("Unable to convert '" + color + "' ro RGB");
+        debug("Unable to convert '" + color + "' ro RGB");
         return;
     }
     write(rgb);
 }
 
-exports.turnOff = function () {
-    console.log("Disabled lights");
+/**
+ * Turn off the LEDs, by setting all pins to low
+ */
+exports.turnOff = () => {
+    debug("Disabled lights");
     write({ r: 0, g: 0, b: 0 });
 }
 
-exports.blink = function (color, time) {
+/**
+ * Blink the LEDs in the given color
+ * @param {string} color - Hex color to blink
+ * @param {int} time - Time in ms to blink
+ */
+exports.blink = (color, time) => {
     rgb = this.hexToRGB(color);
     var enable = true;
     var blinker = setInterval(() => {
-        console.log("Blinking " + color + "!");
+        debug("Blinking " + color + "!");
         if (enable) {
             write(rgb);
         } else {
             this.turnOff();
         }
         enable = !enable;
-    }, 100);
+    }, 150);
 
     setTimeout(() => {
         clearInterval(blinker);
-        console.log("Stopped blinking...");
+        debug("Stopped blinking...");
         this.turnOff();
     }, time);
+}
+
+/**
+ * Setup the module with the given GPIO pins
+ * @param {int} redGPIO - GPIO pin of the red LED
+ * @param {int} greenGPIO - GPIO pin of the green LED
+ * @param {int} blueGPIO - GPIO pin of the blue LED
+ */
+module.exports = (redGPIO, greenGPIO, blueGPIO) => {
+    this.PINS = {
+        RED: new Gpio(redGPIO, { mode: Gpio.OUTPUT }),
+        GREEN: new Gpio(greenGPIO, { mode: Gpio.OUTPUT }),
+        BLUE: new Gpio(blueGPIO, { mode: Gpio.OUTPUT })
+    }
+    return this;
 }
